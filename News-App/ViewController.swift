4e1db54f3b6f0d1611f3d32c6fa2b8604ef62611
Bakhtovar Umarov <umarov_b@auca.kg>
2021-08-21 13:15:31 +0500

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by Bakhtovar Umarov on 21/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

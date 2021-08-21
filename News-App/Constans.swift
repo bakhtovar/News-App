3d1624b919f3767c91c69f2fcfedfaa5ace5b76a
Bakhtovar Umarov <umarov_b@auca.kg>
2021-08-21 16:10:50 +0500

    Some changes

--- News-App/Constans.swift
+++ News-App/Constans.swift
@@ -9,11 +9,8 @@ import Foundation
 import UIKit
 
 struct K {
-   //static let APIKey = "2e98cd9cf2274ad28b71698fafe89c12"
-   //static let APIKey = "e0154366766d416891b915552e9a4578"
-   // static let APIKey = "dce1a396b5ba40b19317b9be747a3d4c"
-   static let APIKey = "068d0bb8e68442748294d69dbef71e6f"
     
+    static let APIKey = "ENTER_YOUR_KEY" // MARK: - ENTER HERE YOUR NEWS API KEY
     static let url = "https://newsapi.org/v2/"
     var total = 4
     var totalPage = 19
